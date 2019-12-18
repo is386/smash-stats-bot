417e45921c2cbade548e92716440a87bfe63595a
is386 <20195347+is386@users.noreply.github.com>
2019-12-18 17:35:19 +0000

    Changed Hero's spell cmd

--- smashstats.py
+++ smashstats.py
@@ -1,8 +1,3 @@
-# TODO
-# DOWNLOAD ALL HITBOXES
-# IMPLEMENT STAT EMBEDS FOR RIDLEY
-# GET ALL STATS
-
 import discord
 from yaml import safe_load as yamlLoad
 
@@ -10,14 +5,12 @@ prefix = "?"
 cmdPath = "characters/%s/commands.yml"
 hboxPath = "characters/%s/hitboxes/"
 embedColor = 00000000
-moveError1 = "The move \"%s\" does not exist."
-moveError2 = "This character does not have the move \"%s\"."
-charError1 = "The character \"%s\" doesn't exist."
-charError2 = "The character \"%s\" has no data yet."
+moveError1 = "The move **%s** does not exist."
+moveError2 = "This character does not have the move **%s**."
+charError1 = "The character **%s** doesn't exist."
+charError2 = "The character **%s** has no data yet."
 hBoxError = "**%s** does not have a hitbox graphic."
 matchMsg = "There are multiple hitboxes for this move. React with the hitbox you would like (Sender Only):\n```%s```"
-forEmoji = '⏩'
-backEmoji = '⏪'
 nums = ['1️⃣', '2️⃣', '3️⃣', '4️⃣', '5️⃣', '6️⃣', '7️⃣', '8️⃣', '9️⃣']
 
 client = discord.Client()
@@ -79,7 +72,7 @@ async def WaitForReaction(req, resp):
         # Checks if the reaction to a message matches the indicated emoji.
         def CheckReaction(reaction, user):
             e = str(reaction.emoji)
-            return (e in nums or e == backEmoji or e == forEmoji) and user == req.author
+            return e in nums and user == req.author
 
         # This loop prevents a bug where if you did two stats cmds and reacted to one of them, 
         # it would send the follow up message to both messages instead of the one that was reacted to.
@@ -92,27 +85,13 @@ async def WaitForReaction(req, resp):
             for r in resp.reactions:
                 users = await r.users().flatten()
                 if r.count > 1 and req.author in users:
-                    if r.emoji == backEmoji:
-                        await resp.remove_reaction(r.emoji, req.author)
-                        return -2
-                    elif r.emoji == forEmoji:
-                        await resp.remove_reaction(r.emoji, req.author)
-                        return -3
-                    else:
-                        n = nums.index(r.emoji)
-                        return n
+                    n = nums.index(r.emoji)
+                    return n
 
     except TimeoutError:
         return -1
 
     return -1
-
-def ChangePage(x, y, cmdData, matching):
-    s = ""
-    for i in range(x, y):
-        m = cmdData[matching[i]]["title"]
-        s += ("\n %d. %s" % (i+1, m))
-    return s
 
 @client.event
 async def on_message(req):
@@ -140,18 +119,20 @@ async def on_message(req):
         await req.channel.send(charError1 % tempChar)
         return
 
+    # Dictionary with command name as the key and the command attributes (title, text, image, etc.) as the values.
+    cmdData = yamlLoad(open(cmdPath % char))
+
     # Parses the move name.
     move = char
     if len(msg) > 2:
         move = "".join(msg[2:]).lower()
-        tempMove = move
-        move = Translate(move, "moveSynonyms.yml")
-        if move == "Invalid":
-            await req.channel.send(moveError1 % tempMove)
-            return
+        if move not in cmdData.keys():
+            tempMove = move
+            move = Translate(move, "moveSynonyms.yml")
+            if move == "Invalid":
+                await req.channel.send(moveError1 % tempMove)
+                return
 
-    # Dictionary with command name as the key and the command attributes (title, text, image, etc.) as the values.
-    cmdData = yamlLoad(open(cmdPath % char))
     if cmdData == None:
         await req.channel.send(charError2 % char)
         return
@@ -162,36 +143,21 @@ async def on_message(req):
     # Checks if the move has multiple hitboxes
     matching = [i for i in cmdData.keys() if move in i]
     if len(matching) > 1:
-        p1 = 0
-        p2 = len(matching) if len(matching) <= 9 else 9
-        s = ChangePage(p1, p2, cmdData, matching)
+        s = ""
+
+        for i in range(len(matching)):
+            m = cmdData[matching[i]]["title"]
+            s += ("\n %d. %s" % (i+1 ,m))
 
         resp = await req.channel.send(matchMsg % s)
 
-        for i in range(p2):
+        for i in range(len(matching)):
             await resp.add_reaction(nums[i])
 
-        if len(matching) > 9:
-            await resp.add_reaction(backEmoji)
-            await resp.add_reaction(forEmoji)
-
-        while True:
-            n = await WaitForReaction(req, resp)
-            if n == -1:
-                return
-            elif n == -2:
-                p1 = p1 - 10 if p1 - 10 >= 0 else 0
-                p2 = p1 + 9 if p1 + 10 >= 9 else 9
-                s = ChangePage(p1, p2, cmdData, matching)
-                await resp.edit(content=matchMsg % s)
-            elif n == -3:
-                p2 = p2 + 10 if p2 + 10 <= len(matching) else len(matching)
-                p1 = p1 + 10 if p1 + 10 <= p2 else p1
-                s = ChangePage(p1, p2, cmdData, matching)
-                await resp.edit(content=matchMsg % s)
-            elif n + p1 <= len(matching):
-                break
-        move = matching[n + p1]
+        n = await WaitForReaction(req, resp)
+        if n == -1:
+            return
+        move = matching[n]
         await resp.delete()
 
     # Sends the message response.
