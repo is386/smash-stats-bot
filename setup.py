98139d557381b722c3034c9a39a0c92076e3fc86
StackWolfed <stackwolfed@protonmail.com>
2020-01-29 10:35:24 +0100

    adds setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='SmashStats',
+    version='0.1',
+    packages=[''],
+    url='https://github.com/is386/SmashStats/blob/refactor/smashstats.py',
+    license='GPL',
+    author='is386',
+    author_email='',
+    description=''
+)
