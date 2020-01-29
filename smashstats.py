7841cba31688449aee20ba313c96a469745e3d05
StackWolfed <stackwolfed@protonmail.com>
2020-01-29 19:41:34 +0100

    readds the client lol

--- smashstats.py
+++ smashstats.py
@@ -15,6 +15,8 @@ matchMsg = "There are multiple hitboxes for this move. React with the hitbox you
 number_emojis = ['1️⃣', '2️⃣', '3️⃣', '4️⃣', '5️⃣', '6️⃣', '7️⃣', '8️⃣', '9️⃣', '🔟']
 cmds = ("{}viz".format(prefix), "{}vis".format(prefix))
 
+client = discord.Client()
+
 
 def translate(name: str, file_path: str) -> str:
     """
