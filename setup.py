06b8fb8198e112bbf6d1b8dd747265900fb1a4a0
StackWolfed <stackwolfed@protonmail.com>
2020-02-01 13:25:25 +0100

    starts adding tests

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='SmashStats',
     version='0.1',
-    packages=[''],
+    packages=find_packages(),
     url='https://github.com/is386/SmashStats/blob/refactor/smashstats.py',
     license='GPL',
     author='is386',
