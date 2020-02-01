d2186d6afe297cd9b6fe85811d76b7ccfc0c07c4
INDERVIR_PC\Indervir <is386@drexel.edu>
2020-02-01 00:51:27 -0500

    add set_prefix command

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from discord.ext import commands
 from smashstats import moveset, embeds
 from secret import token
 
+db_name = "prefixes.db"
 default_prefix = "?"
 status_msg: str = "Type {}help"
 embed_error: str = "An error has occurred during the creation of the embed:\n{}"
@@ -18,15 +19,16 @@ async def get_prefix(bot, ctx) -> str:
     :param ctx: `Context`
     :return: `str`
     """
-    conn: sqlite3.Connection = sqlite3.connect("prefixes.db")
+    conn: sqlite3.Connection = sqlite3.connect(db_name)
     c: sqlite3.Cursor = conn.cursor()
     c = c.execute(
         "SELECT prefix FROM prefixes WHERE server_id={}".format(ctx.guild.id))
+    rows = c.fetchall()
 
-    if c == None:
+    if len(rows) == 0:
         return default_prefix
 
-    p = c.fetchone()[0]
+    p = rows[0][0]
     conn.close()
     return p
 
@@ -68,5 +70,29 @@ async def send_help(ctx: commands.Context):
     await ctx.author.send(help_msg)
     await ctx.send("Sent you a DM {}.".format(ctx.author.mention))
 
+
+@bot.command(name='prefix')
+@commands.has_permissions(administrator=True)
+async def set_prefix(ctx: commands.Context, prefix: str):
+    """
+    Async function to send a direct message with the help text.
+    :param ctx: `Context`
+    :return: `None`
+    """
+    # TODO: If the user does not have permissions, send a message saying they can't use this cmd.
+    # TODO: If the user is missing an argument, tell them the correct format.
+    # TODO: If the server_id exists, then update. Else do an insert. Right now theres only update.
+
+    if len(prefix) > 3:
+        ctx.send("That prefix is too long. It must 3 characters or less.")
+        return
+
+    conn: sqlite3.Connection = sqlite3.connect(db_name)
+    c: sqlite3.Cursor = conn.cursor()
+    c.execute(
+        'UPDATE prefixes SET prefix="{}" WHERE server_id={}'.format(prefix, ctx.guild.id))
+    conn.commit()
+    conn.close()
+    await ctx.send("Your new prefix has been set to `{}`".format(prefix))
 
 bot.run(token)
