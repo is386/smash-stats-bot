790b451b39b19e629ac309779e4ee833279ea563
is386 <20195347+is386@users.noreply.github.com>
2020-02-02 15:34:10 -0500

    changed imports for prefix stuff

--- smashstats/database.py
+++ smashstats/database.py
@@ -1,14 +1,14 @@
-import sqlite3
+from sqlite3 import Connection, Cursor, connect
 
 
 def connect_to_prefix_db(db_name: str):
     """
     Connects to the given DB and creates a prefixes table.
     :param db_name: `str`
-    :return: `sqlite3.Connection`
+    :return: `Connection`
     """
-    conn: sqlite3.Connection = sqlite3.connect(db_name)
-    c: sqlite3.Cursor = conn.cursor()
+    conn: Connection = connect(db_name)
+    c: Cursor = conn.cursor()
     c.execute("""
         CREATE TABLE IF NOT EXISTS prefixes (
             server_id int not NULL,
