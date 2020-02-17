1d4cc5f4d6b881c1a80765e93e3d192a25dfac77
is386 <20195347+is386@users.noreply.github.com>
2020-02-17 14:46:26 -0500

    fix tests

--- tests/smashstats/test_embeds.py
+++ tests/smashstats/test_embeds.py
@@ -1,10 +1,8 @@
 import pytest
-from smashstats import embeds
+from smashstats import embeds, move_model
 
 
 def test_create_image_embed():
-    with pytest.raises(KeyError):
-        char_data = {
-            "test": "test"
-        }
-        embeds.create_image_embed(char_data)
+    move = move_model.Move("test", "title", "url")
+    embed = embeds.create_image_embed(move)
+    assert embed.image.url == "url" and embed.title == "title"
