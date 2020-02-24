60b28010be09fb5c15f9babc7854464a6db6faf3
is386 <is386@drexel.edu>
2020-02-24 18:33:22 -0500

    add ?stats

--- smashstats/embeds.py
+++ smashstats/embeds.py
@@ -4,13 +4,30 @@ from smashstats import move_model
 embed_color: int = 00000000
 
 
-def create_image_embed(move: move_model.Move) -> Embed:
+def create_viz_embed(move: move_model.Move) -> Embed:
     """
-    Create the embed object from the move object..
+    Create the image embed object from the move object.
 
     :param move: `move_model.Move` move's data model
     :return: `Embed` image message to send to the user
     """
     embed: Embed = Embed(title=move.get_title(), color=embed_color)
     embed.set_image(url=move.get_image())
+    return embed
+
+
+def create_stats_embed(move: move_model.Move) -> Embed:
+    """
+    Create the text embed object from the move object.
+
+    :param move: `move_model.Move` move's data model
+    :return: `Embed` text message to send to the user
+    """
+    embed: Embed = Embed(title=move.get_title(), color=embed_color)
+    frame_data: dict = move.get_frame_data()
+
+    for fd in frame_data.keys():
+        if frame_data[fd] is not None:
+            embed.add_field(name=fd, value=frame_data[fd], inline=True)
+
     return embed
