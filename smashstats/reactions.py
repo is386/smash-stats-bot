54f1d471425a75f68e7f8b0e6b9cbb874b363a65
is386 <20195347+is386@users.noreply.github.com>
2020-03-02 10:25:07 -0500

    add 2 more emojis for joker's nb select

--- smashstats/reactions.py
+++ smashstats/reactions.py
@@ -5,8 +5,8 @@ from discord import Message, User
 from discord.ext.commands import Context
 
 timeout = 60.0
-number_emojis: List[str] = ['1️⃣', '2️⃣', '3️⃣', '4️⃣', '5️⃣',
-                            '6️⃣', '7️⃣', '8️⃣', '9️⃣', '🔟']
+number_emojis: List[str] = ['1️⃣', '2️⃣', '3️⃣', '4️⃣', '5️⃣', '6️⃣',
+                            '7️⃣', '8️⃣', '9️⃣', '🔟', '🇦', '🇧']
 red_circle: str = "🔴"
 
 
